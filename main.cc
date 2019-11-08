b9b1740895365b1266f3bfe8914f077ad2a71b67
Peter Chukwuemeka Nwanosike <dexter4life@gmail.com>
2019-11-08 01:24:34 +0100

    Update main.cc

--- main.cc
+++ main.cc
@@ -33,3 +33,37 @@ DWORD FileToVA(DWORD dwFileAddr, PIMAGE_SECTION_HEADER lpSecHdr)
     }
     return NULL;
 }
+
+DWORD OffsetToRVA(DWORD offset, IMAGE_SECTION_HEADER *is_hdr, unsigned scount)
+{
+    // Find section holding the Offset
+    for(unsigned i = 0; i < scount;i++)
+        if((offset >= is_hdr[i].PointerToRawData) && (offset <= is_hdr[i].PointerToRawData +is_hdr[i].SizeOfRawData))
+        {
+            // Convert Offset to RVA
+            return offset+is_hdr[i].VirtualAddress-is_hdr[i].PointerToRawData;
+        }
+    return 0;
+}
+
+/*Convert Virtual Address to File Offset */
+DWORD Rva2Offset(DWORD rva, PIMAGE_SECTION_HEADER psh, PIMAGE_NT_HEADERS pnt)
+{
+	size_t i = 0;
+	PIMAGE_SECTION_HEADER pSeh;
+	if (rva == 0)
+	{
+		return (rva);
+	}
+	pSeh = psh;
+	for (i = 0; i < pnt->FileHeader.NumberOfSections; i++)
+	{
+		if (rva >= pSeh->VirtualAddress && rva < pSeh->VirtualAddress +
+			pSeh->Misc.VirtualSize)
+		{
+			break;
+		}
+		pSeh++;
+	}
+	return (rva - pSeh->VirtualAddress + pSeh->PointerToRawData);
+}
